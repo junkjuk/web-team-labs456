31f21a67c8c7868e5e2f07e3d552d10910edfd13
KosieVich <junkjuk5645@gmail.com>
2024-12-14 22:37:03 +0200

    Fix ui

--- frontend/lab4/src/components/gridComponent.tsx
+++ frontend/lab4/src/components/gridComponent.tsx
@@ -14,10 +14,25 @@ export interface gridComponentProps {
 
 export default function GridComponent(props: gridComponentProps) {
 
-  const gridClass = "grid grid-cols-[200px_repeat(colNumbs,_1fr)] w-full";
+  const gridClass5 = "grid grid-cols-[200px_repeat(5,_1fr)] w-full";
+  const gridClass4 = "grid grid-cols-[200px_repeat(4,_1fr)] w-full";
+  const gridClass3 = "grid grid-cols-[200px_repeat(3,_1fr)] w-full";
+  const gridClass6 = "grid grid-cols-[200px_repeat(6,_1fr)] w-full";
   console.log(props.columns.length, props.columns);
+  const getClass = (numb: number) => {
+    switch (numb) {
+      case 5:
+        return gridClass5;
+      case 4:
+        return gridClass4;
+      case 3:
+        return gridClass3;
+      case 6:
+        return gridClass6;
+    }
+  }
   return (
-    <div className={gridClass.replace("colNumbs", (props.columns.length - 1).toString())}>
+    <div className={getClass(props.columns.length - 1)}>
       <When condition={!!props.columns && props.columns.length !== 0}>
         {() => <>{props.columns?.map(i => <div className="w-fit" key={i}>{i}</div>)}</>}
       </When>
